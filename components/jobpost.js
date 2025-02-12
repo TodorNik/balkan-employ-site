729a7a50f434019928a592db2ae049d368569901
TodorNik <122238741+TodorNik@users.noreply.github.com>
2025-02-13 00:02:20 +0100

    Add jobpost page

---
+++ components/jobpost.js
@@ -0,0 +1,13 @@
+export default function Jobpost() {
+    return (
+        <div>
+            <button>Sacuvaj</button>
+            <button>Ukloni</button>
+            <p>Korisnicko Ime</p>
+            <p>Satnica/Dnevnica</p>&nbsp;<p>Profesija</p>&nbsp;<p>Iskustvo</p>
+            <p>Uopsteni opis posla koji ima maksimum broj karaktera</p>
+
+        </div>
+
+    );
+}
