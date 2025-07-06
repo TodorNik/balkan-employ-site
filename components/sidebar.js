a47c1aecf12bc9a7a7a20a1ba4e11d7038de787b
TodorNik <122238741+TodorNik@users.noreply.github.com>
2025-07-06 13:57:15 +0200

    Add Sidebar

---
+++ components/sidebar.js
@@ -0,0 +1,13 @@
+export default function Sidebar() {
+    return (
+    <div className="bg-gray-800 text-white">
+        <a href="#" className="hover:text-gray-300">Tech</a>
+        <a href="#" className="hover:text-gray-300">Finance</a>
+        <a href="#" className="hover:text-gray-300">Housework</a>
+        <a href="#" className="hover:text-gray-300">Education</a>
+    </div>
+
+
+    );
+  }
+  
