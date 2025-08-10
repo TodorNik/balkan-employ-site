0f2ff5e1b77f1c65ca1735950a5507f6f495da32
TodorNik <122238741+TodorNik@users.noreply.github.com>
2025-08-10 16:24:36 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   ];
 //test
   return (
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div class="layout">
       <Navbar></Navbar>
       <Sidebar></Sidebar>
       <ul className="main grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 list-none p-0 m-0">
