1f0c69e4b60eb1890971dad80ffb82883f9bb57b
TodorNik <122238741+TodorNik@users.noreply.github.com>
2025-08-10 16:21:47 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       <Navbar></Navbar>
       <Sidebar></Sidebar>
-      <ul class="main">
+      <ul className="main grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 list-none p-0 m-0">
       {Array.isArray(jobPosts) &&
     jobPosts.map((job) => (
       <li class ="complist" key={job.id}>
