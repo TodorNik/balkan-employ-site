4efb81ba636ea14f2d9340fbac0bee007d756a35
TodorNik <122238741+TodorNik@users.noreply.github.com>
2025-08-10 16:29:26 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,6 +8,8 @@ export default function Home() {
     { id: 1, title: "Frontend Developer", description: "Develop awesome UIs with React." },
     { id: 2, title: "Backend Developer", description: "Build robust APIs with Node.js." },
     { id: 3, title: "Fullstack Developer", description: "Work across the entire stack." },
+    { id: 4, title: "Backend Developer", description: "Build robust APIs with Node.js." },
+    { id: 5, title: "Fullstack Developer", description: "Work across the entire stack." },
   ];
 //test
   return (
