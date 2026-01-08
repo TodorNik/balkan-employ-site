13fecfcd68df1ae1277e350f5b95ad1663fb025e
TodorNik <122238741+TodorNik@users.noreply.github.com>
2026-01-08 17:52:05 +0100

    Update route.js

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,5 +1,5 @@
 // app/api/login/route.js
-import { getSupabaseClient } from 'app/lib/supabaseClient';
+import { getSupabaseClient } from '../../../../lib/supabaseClient';
 
 export async function POST(req) {
   const { email, password } = await req.json();
